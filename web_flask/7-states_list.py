b6de375d13dcf534a9f12d7f3ef48b8b0ef69757
diegovivas <dieguitovivas@hotmail.com>
2020-01-21 01:14:39 +0000

    [feature] task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+run flask app
+"""
+from models import storage
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    lista = storage.all('State')
+    return render_template('7-states_list.html', lista=lista)
+
+
+@app.teardown_appcontext
+def close_(self):
+    storage.close()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
